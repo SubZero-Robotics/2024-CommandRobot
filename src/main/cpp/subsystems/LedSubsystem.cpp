5204d8da81e0469d7a727a49336547ed53de6c1a
nobody5050 <levibelland@gmail.com>
2024-01-17 21:24:53 -0600

    ConnectorX pattern

--- src/main/cpp/subsystems/LedSubsystem.cpp
+++ src/main/cpp/subsystems/LedSubsystem.cpp
@@ -3,20 +3,25 @@
 
 void LedSubsystem::intakingLED() {
     m_connectorX.setColor(LedConstants::kIntakeLedPort, ColorConstants::kRed);
+    m_connectorX.setPattern(LedConstants::kIntakeLedPort, PatternType::Blink);
 }
 
 void LedSubsystem::scoringSpeakerLED() {
     m_connectorX.setColor(LedConstants::kSpeakerLedPort, ColorConstants::kGreen);
+    m_connectorX.setPattern(LedConstants::kSpeakerLedPort, PatternType::Blink);
 }
 
 void LedSubsystem::scoringAmpLED() {
     m_connectorX.setColor(LedConstants::kAmpLedPort, ColorConstants::kBlue);
+    m_connectorX.setPattern(LedConstants::kAmpLedPort, PatternType::Blink);
 }
 
 void LedSubsystem::stowingLED() {
     m_connectorX.setColor(LedConstants::kStowLedPort, ColorConstants::kPurple);
+    m_connectorX.setPattern(LedConstants::kStowLedPort, PatternType::Breathe);
 }
 
 void LedSubsystem::idlingLED() {
     m_connectorX.setColor(LedConstants::kIdleLedPort, ColorConstants::kYellow);
+    m_connectorX.setPattern(LedConstants::kIdleLedPort, PatternType::SetAll);
 }
