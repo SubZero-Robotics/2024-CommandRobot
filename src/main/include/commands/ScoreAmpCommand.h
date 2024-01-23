9193e8cefaaa42b4c9c7c711d0cd88c061799e56
5690 <5690Programmers@gmail.com>
2024-01-22 19:20:15 -0600

    Add amp and speaker scoring commands :flushed:

---
+++ src/main/include/commands/ScoreAmpCommand.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <frc2/command/Command.h>
+#include <frc2/command/CommandHelper.h>
+
+#include "subsystems/ScoringSubsystem.h"
+#include "subsystems/IntakeSubsystem.h"
+
+class ScoreAmp : public frc2::CommandHelper<frc2::Command, ScoreAmp> {
+    public:
+
+        enum class ScoreState {
+            FlywheelRamp,
+            Feeding,
+        };
+
+        explicit ScoreAmp(ScoringSubsystem* subsystem, IntakeSubsystem* intk) 
+            : m_score{subsystem}, m_intake{intk}, isFinished{false} {
+                AddRequirements(m_intake);
+                AddRequirements(m_score);
+            }
+
+        void Initialize() override {
+            if (!m_intake->NotePresent()) {
+                isFinished = true;
+                return;
+            }
+            m_state = ScoreState::FlywheelRamp;
+            m_score->StartScoringRamp(ScoringDirection::AmpSide);
+            isFinished = false;
+        }
+
+        void Execute() override {
+            if (m_state == ScoreState::FlywheelRamp) {
+                if (m_score->GetMotorAtScoringSpeed(ScoringDirection::AmpSide)) {
+                    m_score->SpinVectorSide(ScoringDirection::AmpSide);
+                    m_intake->In();
+                    m_state = ScoreState::Feeding;
+                }
+            } else if (m_state == ScoreState::Feeding) {
+                if (m_score->GetMotorFreeWheel(ScoringDirection::AmpSide)) {
+                    isFinished = true;
+                }
+            } else {
+                isFinished = true;
+            }
+        }
+
+        void End(bool interrupted) override {
+            m_intake->Stop();
+            m_score->Stop();
+        }
+
+        bool IsFinished() override {
+            return isFinished;
+        }
+
+    private:
+        ScoringSubsystem* m_score;
+        IntakeSubsystem* m_intake;
+        bool isFinished;
+        ScoreState m_state;
+};
