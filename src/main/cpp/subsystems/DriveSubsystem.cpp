7fed073f8eabdb71f75cb679517cae25309d5582
5690 <5690Programmers@gmail.com>
2024-02-26 14:21:12 -0600

    clean up commented out drive code

--- src/main/cpp/subsystems/DriveSubsystem.cpp
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -94,28 +94,11 @@ void DriveSubsystem::Periodic() {
     auto visionPoses = m_vision->UpdateEstimatedGlobalPose(poseEstimator);
 
     auto updatedPose = poseEstimator.GetEstimatedPosition();
-    // Might need to get the pose from somewhere else?
-    // frc::Pose2d finalPose = m_lastGoodPosition;
-    // ? Do we need to flip this first based on alliance?
     // https://github.com/Hemlock5712/2023-Robot/blob/dd5ac64587a3839492cfdb0a28d21677d465584a/src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java#L149
-
-    // if (updatedPose.X() > 0_m && updatedPose.X() <= 100_m &&
-    //     updatedPose.Y() > 0_m && updatedPose.Y() <= 100_m) {
       ConsoleLogger::getInstance().logVerbose(
           "DriveSubsystem PoseEstimator final pose", updatedPose);
       m_lastGoodPosition = updatedPose;
       m_field.SetRobotPose(updatedPose);
-    //}
-    // else {
-    //   auto cam1Est = visionPoses.first;
-    //   auto cam2Est = visionPoses.second;
-
-    //   if (cam1Est.has_value()) {
-    //     finalPose = cam1Est.value().estimatedPose.ToPose2d();
-    //   } else if (cam2Est.has_value()) {
-    //     finalPose = cam2Est.value().estimatedPose.ToPose2d();
-    //   }
-    // }
   };
 }
 
@@ -123,113 +106,6 @@ void DriveSubsystem::Drive(units::meters_per_second_t xSpeed,
                            units::meters_per_second_t ySpeed,
                            units::radians_per_second_t rot, bool fieldRelative,
                            bool rateLimit, units::second_t periodSeconds) {
-  // double xSpeedCommanded;
-  // double ySpeedCommanded;
-
-  // ShuffleboardLogger::getInstance().logInfo("xSpeed", xSpeed.value());
-  // ShuffleboardLogger::getInstance().logInfo("ySpeed", ySpeed.value());
-  // ShuffleboardLogger::getInstance().logInfo("Rotation", rot.value());
-
-  // double currentTime = wpi::Now() * 1e-6;
-  // double elapsedTime = currentTime - m_prevTime;
-  // periodSeconds = units::second_t(elapsedTime);
-
-  // if (rateLimit) {
-  //   // Convert XY to polar for rate limiting
-  //   double inputTranslationDir = atan2(ySpeed.value(), xSpeed.value());
-  //   double inputTranslationMag =
-  //       sqrt(pow(xSpeed.value(), 2) + pow(ySpeed.value(), 2));
-
-  //   // Calculate the direction slew rate based on an estimate of the lateral
-  //   // acceleration
-  //   double directionSlewRate;
-  //   if (m_currentTranslationMag != 0.0) {
-  //     directionSlewRate =
-  //         abs(DriveConstants::kDirectionSlewRate / m_currentTranslationMag);
-  //   } else {
-  //     directionSlewRate = 500.0;  // some high number that means the slew
-  //     rate
-  //                                 // is effectively instantaneous
-  //   }
-
-  //   double currentTime = wpi::Now() * 1e-6;
-  //   double elapsedTime = currentTime - m_prevTime;
-  //   double angleDif = SwerveUtils::AngleDifference(inputTranslationDir,
-  //                                                  m_currentTranslationDir);
-
-  //   driveLoopTime = units::second_t{elapsedTime};
-
-  //   if (angleDif < 0.45 * std::numbers::pi) {
-  //     m_currentTranslationDir = SwerveUtils::StepTowardsCircular(
-  //         m_currentTranslationDir, inputTranslationDir,
-  //         directionSlewRate * elapsedTime);
-  //     m_currentTranslationMag = m_magLimiter.Calculate(inputTranslationMag);
-  //   } else if (angleDif > 0.85 * std::numbers::pi) {
-  //     if (m_currentTranslationMag >
-  //         1e-4) {  // some small number to avoid floating-point errors with
-  //                  // equality checking
-  //       // keep currentTranslationDir unchanged
-  //       m_currentTranslationMag = m_magLimiter.Calculate(0.0);
-  //     } else {
-  //       m_currentTranslationDir =
-  //           SwerveUtils::WrapAngle(m_currentTranslationDir +
-  //           std::numbers::pi);
-  //       m_currentTranslationMag =
-  //       m_magLimiter.Calculate(inputTranslationMag);
-  //     }
-  //   } else {
-  //     m_currentTranslationDir = SwerveUtils::StepTowardsCircular(
-  //         m_currentTranslationDir, inputTranslationDir,
-  //         directionSlewRate * elapsedTime);
-  //     m_currentTranslationMag = m_magLimiter.Calculate(0.0);
-  //   }
-  //   m_prevTime = currentTime;
-
-  //   ConsoleLogger::getInstance().logVerbose("DriveSubsystem", "set prev time
-  //   = %f", m_prevTime);
-
-  //   xSpeedCommanded = m_currentTranslationMag * cos(m_currentTranslationDir);
-  //   ySpeedCommanded = m_currentTranslationMag * sin(m_currentTranslationDir);
-  //   m_currentRotation = m_rotLimiter.Calculate(rot.value());
-
-  // } else {
-  //   xSpeedCommanded = xSpeed.value();
-  //   ySpeedCommanded = ySpeed.value();
-  //   m_currentRotation = rot.value();
-  // }
-
-  // // Convert the commanded speeds into the correct units for the drivetrain
-  // units::meters_per_second_t xSpeedDelivered =
-  //     xSpeedCommanded * DriveConstants::kMaxSpeed;
-  // units::meters_per_second_t ySpeedDelivered =
-  //     ySpeedCommanded * DriveConstants::kMaxSpeed;
-  // units::radians_per_second_t rotDelivered =
-  //     m_currentRotation * DriveConstants::kMaxAngularSpeed;
-
-  // ConsoleLogger::getInstance().logInfo("DriveSubsystem", "Current rotation
-  // %f",
-  //                                      m_currentRotation);
-
-  // fieldRelative = true;
-
-  // auto states = kDriveKinematics.ToSwerveModuleStates(
-  //     fieldRelative
-  //         ? frc::ChassisSpeeds::Discretize(
-  //               frc::ChassisSpeeds::FromFieldRelativeSpeeds(
-  //                   xSpeedDelivered, ySpeedDelivered, rotDelivered,
-  //                   frc::Rotation2d(units::degree_t{m_gyro.GetAngle()})),
-  //               driveLoopTime)
-  //         : frc::ChassisSpeeds{xSpeedDelivered, ySpeedDelivered,
-  //         rotDelivered});
-
-  // kDriveKinematics.DesaturateWheelSpeeds(&states, DriveConstants::kMaxSpeed);
-
-  // auto [fl, fr, bl, br] = states;
-
-  // m_frontLeft.SetDesiredState(fl);
-  // m_frontRight.SetDesiredState(fr);
-  // m_rearLeft.SetDesiredState(bl);
-  // m_rearRight.SetDesiredState(br);
 
   auto now = frc::Timer::GetFPGATimestamp();
   auto dif = now - driveLoopTime;
