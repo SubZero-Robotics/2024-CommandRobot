3984c41b830915a474875c586c77d1f6f2bd0d2b
having11 <gatoninja236@gmail.com>
2024-04-13 00:46:10 -0500

    Use WPI functions for rotating the pose

--- src/main/cpp/utils/TargetTracker.cpp
+++ src/main/cpp/utils/TargetTracker.cpp
@@ -122,18 +122,24 @@ std::optional<frc::Pose2d> TargetTracker::GetBestTargetPose(
   units::radian_t verticalAngle = verticalDelta.convert<units::radian>();
   units::radian_t horizontalAngle = bestTarget.centerX.convert<units::radian>();
   units::inch_t distance = heightDelta / tan(verticalAngle.value());
-  // distance -= kDistanceGap;
 
   frc::Pose2d currentPose = m_drive->GetPose();
 
   auto xTransformation = distance * tan(horizontalAngle.value());
   auto yTransformation = distance;
-  // auto yTransformation =
-  //     units::inch_t(pow(cos(horizontalAngle.value()) / distance.value(),
-  //     -1));
+  auto hypotDistance =
+      units::inch_t(pow(cos(horizontalAngle.value()) / distance.value(), -1));
+
   frc::Transform2d transformDelta =
-      frc::Transform2d(xTransformation, yTransformation, 0_deg);
+      frc::Transform2d(-xTransformation, yTransformation, 0_deg);
   frc::Pose2d notePose = currentPose.TransformBy(transformDelta);
+
+  // Do it the WPI way - currently just used for testing
+  auto wpiTranslation = frc::Translation2d(
+      hypotDistance.convert<units::meter>(), frc::Rotation2d(horizontalAngle));
+  auto wpiTransformation =
+      frc::Transform2d(wpiTranslation, currentPose.Rotation());
+  auto wpiFinalPose = currentPose.TransformBy(wpiTransformation);
 
   // Pivot around our robot's current pose to be field-oriented
   double s = sin(currentPose.Rotation().Radians().value());
